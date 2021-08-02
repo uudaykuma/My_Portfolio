7ecb3a04e76061cb61b637c300d2c7a05ca8ee5d
uudaykuma <uudaykuma@gmail.com>
2021-08-02 08:07:12 +0530

    all the pages are added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,169 +1,173 @@
 import React from "react";
 import "../styles/project.css";
 const Projects = () => {
-    return (
-        <div className="project-section">
-            <h1> PROJECTS</h1>
-            <div class="album py-5">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="card mb-4 box-shadow">
-                                <img
-                                    className="project-image"
-                                    src="https://i.ibb.co/GHFfYLG/Screenshot-960.png"
-                                    alt=""
-                                />
-                                <div class="card-body">
-                                    <h3>Premier league clone</h3>
-                                    <p class="card-text">
-                                        The Premier League, often referred to as the English Premier
-                                        League or the EPL (legal name: The Football Association
-                                        Premier League Limited), is the top level of the English
-                                        football league system.
-                                    </p>
+  return (
+    <div className="project-section">
+      <h1> PROJECTS</h1>
+      <div class="album py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img
+                  className="project-image"
+                  src="https://i.ibb.co/GHFfYLG/Screenshot-960.png"
+                  alt=""
+                />
+                <div class="card-body">
+                  <h3>Premier league clone</h3>
+                  <p class="card-text">
+                    The Premier League, often referred to as the English Premier
+                    League or the EPL (legal name: The Football Association
+                    Premier League Limited), is the top level of the English
+                    football league system.
+                  </p>
 
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://github.com/Mangeshbk/Premier-League-clone"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Source code
-                                            </a>{" "}
-                                            &nbsp;&nbsp;
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://cloning-premierleague.netlify.app/"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Demo
-                                            </a>
-                                            &nbsp;&nbsp;
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://medium.com/@uudaykuma/premier-league-clone-5a7a409a7e6e"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Blog
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card mb-4 box-shadow">
-                                <img
-                                    className="project-image"
-                                    src="https://i.ibb.co/6DCh4Jz/Screenshot-959.png"
-                                    alt=""
-                                />
-                                <div class="card-body">
-                                    <h3>Kindemeal.my clone</h3>
-                                    <p class="card-text">
-                                        Kindmeal.my is a website that has a lot of information on everything that is related to food with a wide variety of information.It also provides latest news and other information that is going around the world.
-                                    </p>
-
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://github.com/abhi5iet/Ganga"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Source code
-                                            </a>{" "}
-                                            &nbsp;&nbsp;
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://kindmeal-clone.vercel.app/"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Demo
-                                            </a>
-                                            &nbsp;&nbsp;
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://uday97.hashnode.dev/kindmealmy-clone"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Blog
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card mb-4 box-shadow">
-                                <img
-                                    className="project-image"
-                                    src="https://i.ibb.co/wW8XpGg/Screenshot-951.png"
-                                    alt=""
-                                />
-                                <div class="card-body">
-                                    <h3>Google's digital garage clone</h3>
-                                    <p class="card-text">
-                                        The Digital Garage is a non-profit nationwide programme from Google delivering free digital skills training via an online learning platform.
-                                        <br />
-                                        <br />
-                                    </p>
-
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://github.com/TusharTaral/beas_Learndigital/tree/master/final"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Source code
-                                            </a>{" "}
-                                            &nbsp;&nbsp;
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://google-digital-garage.vercel.app/"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Demo
-                                            </a>
-                                            &nbsp;&nbsp;
-                                            <a
-                                                type="button"
-                                                class="btn btn-sm btn-outline-secondary"
-                                                href="https://uday97.hashnode.dev/google-digital-garage-clone"
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                Blog
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://github.com/Mangeshbk/Premier-League-clone"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Source code
+                      </a>{" "}
+                      &nbsp;&nbsp;
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://cloning-premierleague.netlify.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Demo
+                      </a>
+                      &nbsp;&nbsp;
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://medium.com/@uudaykuma/premier-league-clone-5a7a409a7e6e"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Blog
+                      </a>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img
+                  className="project-image"
+                  src="https://i.ibb.co/6DCh4Jz/Screenshot-959.png"
+                  alt=""
+                />
+                <div class="card-body">
+                  <h3>Kindemeal.my clone</h3>
+                  <p class="card-text">
+                    Kindmeal.my is a website that has a lot of information on
+                    everything that is related to food with a wide variety of
+                    information.It also provides latest news that is going around the world.
+                  </p>
+
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://github.com/abhi5iet/Ganga"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Source code
+                      </a>{" "}
+                      &nbsp;&nbsp;
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://kindmeal-clone.vercel.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Demo
+                      </a>
+                      &nbsp;&nbsp;
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://uday97.hashnode.dev/kindmealmy-clone"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Blog
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img
+                  className="project-image"
+                  src="https://i.ibb.co/wW8XpGg/Screenshot-951.png"
+                  alt=""
+                />
+                <div class="card-body">
+                  <h3>Google's digital garage clone</h3>
+                  <p class="card-text">
+                    The Digital Garage is a non-profit nationwide programme from
+                    Google delivering free digital skills training via an online
+                    learning platform.
+                    <br />
+                    <br />
+                  </p>
+
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://github.com/TusharTaral/beas_Learndigital/tree/master/final"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Source code
+                      </a>{" "}
+                      &nbsp;&nbsp;
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://google-digital-garage.vercel.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Demo
+                      </a>
+                      &nbsp;&nbsp;
+                      <a
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                        href="https://uday97.hashnode.dev/google-digital-garage-clone"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Blog
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
